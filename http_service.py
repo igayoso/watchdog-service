2f4ba5898b458512d81d4416e5462ec7f0fe85ce
igayoso <igayoso@gmail.com>
2014-11-26 01:27:24 +0100

    Initial commit

---
+++ http_service.py
@@ -0,0 +1,92 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import datetime
+import json
+import os
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        # Only continue if there is a path after url but if there is
+        # not path will show message
+        if self.path is not '/':
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            seconds = self.path.strip("/")
+            if seconds.isdigit():
+                self.wfile.write("%s" % get_json(seconds))
+            else:
+                self.wfile.write("Path must be a integer (seconds)")
+        else:
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("You must specify seconds on url path")
+        return
+
+def get_median(data_list):
+    """
+    If using python3 I could use median function but in python 2.7 it's
+    not available, so I toke this median function from github:
+    https://github.com/bycoffe/python-math/blob/master/calculate/median.py
+    """
+    data_list = map(float, data_list)
+    n = len(data_list)
+    data_list.sort()
+    # Test whether the n is odd
+    if n & 1:
+      # If is is, get the index simply by dividing it in half
+      index = n / 2 
+      return data_list[index]
+    else:
+      # If the n is even, average the two values at the center
+      low_index = n / 2 - 1
+      high_index = n / 2
+      average = (data_list[low_index] + data_list[high_index]) / 2
+      return average
+
+def get_json(seconds):
+    """
+    This function return final json that will show webserver. Function
+    get seconds that put in path request, access log file creation and
+    get files
+    """
+    files = [] 
+    filenames_lenght = []
+    json_file = [] 
+    creations_file = open('/var/log/newfiles.log', 'r')
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    date_before = int(now) - int(seconds)
+    # Loop in file to get all lines 
+    for line in creations_file:
+        (key, val) = line.split()
+        check_date_time = int(key)
+        # Check between two datatimes because could be posibility of
+        # new files creation after request
+        if date_before <= check_date_time <= now:
+            path = os.path.basename(val)
+            lenght = len(val)
+            files.append(path)
+            filenames_lenght.append(lenght)
+    # It's only for return json empty data and not get an error
+    if files:
+        median = get_median(filenames_lenght)
+    else:
+        median = ''
+    json_file.append({'files': files, 'median_lenght': median})
+    return(json.dumps(json_file))
+
+def write_pidfile():
+    # FIX: Check if there is a pidfile for don't try start another server
+    pid = str(os.getpid())
+    f = open('/var/run/http_service.pid', 'w')
+    f.write(pid)
+    f.close()
+
+try:
+    write_pidfile()
+    server = HTTPServer(('localhost', 8888), MyHandler)
+    # FIX: send this messages to external log
+    print('Started http server')
+    server.serve_forever()
+except KeyboardInterrupt:
+    print('^C received, shutting down server')
+    server.socket.close()
---
+++ watchdog.py
@@ -0,0 +1,26 @@
+import pyinotify
+import datetime
+import re
+import sys
+
+class TrackCreation(pyinotify.ProcessEvent):
+   def process_IN_CREATE(self, event):
+       filename = event.pathname
+       # Only add entry to log if filename start with _
+       regexp = re.compile(r'_.*')
+       if regexp.search(filename) is not None:
+            # Using better datetime format for help when make operations with datetimes
+            fo.write("%s %s" % (datetime.datetime.now().strftime("%Y%m%d%H%M%S"), filename))
+            fo.flush()
+
+fo = file('/var/log/newfiles.log', 'a')
+try:
+    wm = pyinotify.WatchManager()
+    handler = TrackCreation(fileobj=fo)
+    notifier = pyinotify.Notifier(wm, default_proc_fun=handler)
+    # Only watch for create files, ignore rest of operations
+    wm.add_watch('/home', pyinotify.IN_CREATE)
+    # Daemonize for start with init.d script
+    notifier.loop(daemonize=True, pid_file='/var/run/whatchdog.pid')
+except pyinotify.NotifierError, err:
+    print >> sys.stderr, err
